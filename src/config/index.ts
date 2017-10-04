f6f103d2ac15736e76e697851063f48d12e4ca1f
Cedric Charly <cedric.charly@gmail.com>
2017-10-04 01:51:45 -0500

    config: load config through environment variables
    Config will be read at runtime from environment variables and loaded
    into config object. Invalid config will force application exit.

---
+++ src/config/index.ts
@@ -0,0 +1,24 @@
+import * as joi from 'joi'
+
+const schema = joi.object({
+  FORUM_DATABASE_URI: joi
+    .string()
+    .uri({ scheme: ['mysql'] })
+    .required(),
+  LOG_LEVEL: joi
+    .string()
+    .allow(['trace', 'debug', 'info', 'warn', 'error', 'fatal'])
+    .default('debug'),
+})
+
+const { error, value: configVars } = joi.validate(process.env, schema, {
+  allowUnknown: true,
+})
+if (error) {
+  throw new Error(`config validation error: ${error.message}`)
+}
+
+export const config = {
+  forumDatabaseURI: configVars.FORUM_DATABASE_URI,
+  logLevel: configVars.LOG_LEVEL,
+}
